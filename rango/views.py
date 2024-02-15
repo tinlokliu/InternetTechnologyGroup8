eb4daa26fde18e189e0ed7b85709d00da71ddcb1
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-02-15 22:22:23 +0000

    Added the mainpage view

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def mainpage(request):
+    return HttpResponse("This will be the mainpage for our Cooking Website!")
