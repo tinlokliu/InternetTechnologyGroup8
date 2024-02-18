bee4a9a1d04b785f514c2471b8760455c8ab7419
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-02-18 17:16:52 +0000

    temporary fix, website should work now with temporary HTML

--- InternetTechnologyGroup8/settings.py
+++ InternetTechnologyGroup8/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'InternetTechnologyGroup8.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Itech/InternetTechnologyGroup8/templates'],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("This will be the mainpage for our Cooking Website!")
+   ### return HttpResponse("This will be the mainpage for our Cooking Website!")
+    return render(request, 'rango/mainpage.html')
