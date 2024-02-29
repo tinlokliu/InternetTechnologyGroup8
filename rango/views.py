67a69efb4165e6d6d925d9fc14f32efa73c7d1e7
Lila Mrowiec <b9063187@newcastle.ac.uk>
2024-02-29 15:42:13 +0000

    Uploading Homepage and Receipe Catlogue

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, 'rango/mainpage.html')
+    return render(request, 'rango/homepage1.html')
 
 def about(request):
     return render(request, 'rango/about.html')
