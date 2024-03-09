c5621f798054b9fe1ecfeb98b73f5324f7b25e2d
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-03-09 17:03:45 +0000

    added database following tango with django book

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from rango.models import UserProfile
+from rango.models import UserProfile, Recipe
+
+admin.site.register(UserProfile)
+admin.site.register(Recipe)
--- rango/models.py
+++ rango/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class UserProfile(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+class Recipe(models.Model):
+    recipe_ID = models.CharField(max_length=255, primary_key=True)
+    recipe_title = models.CharField(max_length=255, null=False)
 
-    def __str__(self):
-        return self.user.username
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255, null=False)
+    password = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)
+    recipe_ID = models.ForeignKey(Recipe, on_delete=models.SET_NULL, null=True, blank=True)
